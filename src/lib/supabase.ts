6e44ca3c4efd42a46c4896b285efc69ad115e75c
malatesta-creator <malatesta@open2.it>
2025-12-16 10:33:40 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://emfvodkfnttnphxqwnsd.supabase.co'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtZnZvZGtmbnR0bnBoeHF3bnNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzUxOTcsImV4cCI6MjA3OTc1MTE5N30.7gpxlj-UJ-SIqCeFqR5olY5Tsh41jcO5anq2Grn2Cv4';
 
-export const supabase = createClient(
-  supabaseUrl || '',
-  supabaseAnonKey || ''
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
