34e3be3f50994465979b74046a498c7aaa74a0cd
malatesta-creator <malatesta@open2.it>
2025-12-16 10:11:05 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,15 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-// ------------------------------------------------------------------
-// CONFIGURAZIONE SUPABASE (DATABASE)
-// ------------------------------------------------------------------
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// 1. Incolla qui il "Project URL" (trovato su Supabase > API Settings)
-const supabaseUrl = 'https://emfvodkfnttnphxqwnsd.supabase.co'; 
-
-// 2. Incolla qui la "Publishable Key" (trovata su Supabase > API Settings)
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtZnZvZGtmbnR0bnBoeHF3bnNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzUxOTcsImV4cCI6MjA3OTc1MTE5N30.7gpxlj-UJ-SIqCeFqR5olY5Tsh41jcO5anq2Grn2Cv4';
-
-// Crea la connessione al database
-// @ts-ignore
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  supabaseUrl || '',
+  supabaseAnonKey || ''
+);
